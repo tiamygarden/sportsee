5832232eb680cf639a2e22e8aa3a3e26bf211607
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-06-08 10:13:47 +0200

    Suppression des fichiers obsoletes

--- src/model/NutritionData.jsx
+++
@@ -1,8 +0,0 @@
-export default class NutritionData {
-    constructor(calorieCount, proteinCount, carbohydrateCount, lipidCount) {
-        this.calorieCount = calorieCount
-        this.proteinCount = proteinCount
-        this.carbohydrateCount = carbohydrateCount
-        this.lipidCount = lipidCount
-    }
-}
